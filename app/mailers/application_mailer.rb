19412b4253285565f0d508e15a834370266eff42
jeanarm <nizigiarmel2015@gmail.com>
2019-09-03 09:57:08 +0200

    Second commit

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'nizigiarmel2015@gmail.com'
+  default from: 'aminauzayisenga@gmail.com'
   layout 'mailer'
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
     def contact_mail(contact)
         @contact = contact
       
-        mail to: "nizigiarmel2015@gmail.com", subject: "confirmation e-mail of inquiry"
+        mail to: "example@gmail.com", subject: "confirmation e-mail of inquiry"
 end
 end
